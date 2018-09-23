b9a14ba1afda4b21771120228e8eef4aad7a23dc
David Sulc <davidsulc@gmail.com>
2018-09-23 15:32:22 +0200

    refactor

--- lib/endpoints/app/jobs.ex
+++ lib/endpoints/app/jobs.ex
@@ -6,32 +6,43 @@ defmodule SHEx.Endpoints.App.Jobs do
 
   @base_url "https://app.scrapinghub.com/api"
 
-  def delete(api_key, project_id, jobs, opts \\ [])
+  def delete(api_key, project_id, job_or_jobs, opts \\ [])
       when is_api_key(api_key)
       when is_project_id(project_id)
-      when is_list(jobs)
+      when is_job_id(job_or_jobs) or is_list(job_or_jobs)
       when is_list(opts) do
-    RequestConfig.new()
-    |> Map.put(:api_key, api_key)
-    |> Map.put(:url, "#{@base_url}/jobs/delete.json?project=#{project_id}")
-    |> Map.put(:method, :post)
-    |> Map.put(:body, jobs |> Enum.map(&{:job, &1}))
-    |> Map.put(:opts, opts)
+    prepare_stop_or_delete_request(api_key, project_id, job_or_jobs, opts)
+    |> Map.put(:url, "#{@base_url}/jobs/delete.json")
     |> make_request()
   end
 
-  def stop(api_key, project_id, jobs, opts \\ [])
+  def stop(api_key, project_id, job_or_jobs, opts \\ [])
       when is_api_key(api_key)
       when is_project_id(project_id)
-      when is_list(jobs)
+      when is_job_id(job_or_jobs) or is_list(job_or_jobs)
       when is_list(opts) do
+    prepare_stop_or_delete_request(api_key, project_id, job_or_jobs, opts)
+    |> Map.put(:url, "#{@base_url}/jobs/stop.json")
+    |> make_request()
+  end
+
+  defp prepare_stop_or_delete_request(api_key, project_id, job_or_jobs, opts) do
+    body =
+      job_or_jobs
+      |> format_jobs()
+      |> Keyword.put(:project, project_id)
+
     RequestConfig.new()
     |> Map.put(:api_key, api_key)
-    |> Map.put(:url, "#{@base_url}/jobs/stop.json?project=#{project_id}")
     |> Map.put(:method, :post)
-    |> Map.put(:body, jobs |> Enum.map(&{:job, &1}))
+    |> Map.put(:body, body)
     |> Map.put(:opts, opts)
-    |> make_request()
+  end
+
+  defp format_jobs(job_or_jobs) do
+    job_or_jobs
+    |> List.wrap()
+    |> Enum.map(&{:job, &1})
   end
 
   defp make_request(%RequestConfig{opts: opts} = config) do
--- lib/endpoints/guards.ex
+++ lib/endpoints/guards.ex
@@ -2,4 +2,6 @@ defmodule SHEx.Endpoints.Guards do
   defguard is_api_key(key) when is_binary(key)
 
   defguard is_project_id(id) when is_binary(id) or is_integer(id)
+
+  defguard is_job_id(id) when is_binary(id)
 end
