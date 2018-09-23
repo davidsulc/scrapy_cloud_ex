38cf3b2e733305f5269b64bd21bacde9217d4b07
David Sulc <davidsulc@gmail.com>
2018-09-23 17:22:54 +0200

    implemented Jobs.run

--- lib/endpoints/app/jobs.ex
+++ lib/endpoints/app/jobs.ex
@@ -7,6 +7,33 @@ defmodule SHEx.Endpoints.App.Jobs do
   @base_url "https://app.scrapinghub.com/api"
   @valid_update_params [:add_tag, :remove_tag]
 
+  def run(api_key, project_id, spider_name, params \\ [], opts \\ [])
+      when is_api_key(api_key)
+      when is_project_id(project_id)
+      when is_binary(spider_name)
+      when is_list(params)
+      when is_list(opts) do
+    with job_settings <- params |> Keyword.get(:job_settings),
+         json_encoder <- opts |> Keyword.get(:json_encoder),
+         {:ok, job_settings} <- format_job_settings(job_settings, json_encoder) do
+      body =
+        params
+        |> Keyword.put(:project, project_id)
+        |> Keyword.put(:spider, spider_name)
+        |> maybe_add_job_settings(job_settings)
+
+      RequestConfig.new()
+      |> Map.put(:api_key, api_key)
+      |> Map.put(:method, :post)
+      |> Map.put(:body, body)
+      |> Map.put(:opts, opts)
+      |> Map.put(:url, "#{@base_url}/run.json")
+      |> make_request()
+    else
+      error -> {:error, error}
+    end
+  end
+
   def update(api_key, project_id, job_or_jobs, params \\ [], opts \\ [])
       when is_api_key(api_key)
       when is_project_id(project_id)
@@ -73,6 +100,34 @@ defmodule SHEx.Endpoints.App.Jobs do
 
   defp get_http_client(opts) do
     opts |> Keyword.get(:http_adapter, DefaultAdapter)
+  end
+
+  defp format_job_settings(nil, _encoder), do: {:ok, []}
+
+  defp format_job_settings(settings, _encoder) when is_binary(settings), do: settings
+
+  defp format_job_settings(settings, _encoder = nil) when is_map(settings) do
+    if function_exported?(Jason, :encode, 2) do
+      format_job_settings(settings, &Jason.encode(&1, []))
+    else
+      {:invalid_params,
+       {:unencoded_job_settings_without_encoder,
+        "job_settings must be provided as a string-encoded JSON object, or a JSON encoder must be provided as an option (falling back to Jason unsuccessful)"}}
+    end
+  end
+
+  # TODO: document that encoder must return {:ok, encoded_json} on success
+  defp format_job_settings(settings, encoder) when is_map(settings), do: encoder.(settings)
+
+  defp format_job_settings(_settings, _encoder) do
+    {:invalid_params,
+     {:job_settings, "expected job_settings to be a string-encoded JSON object or a map"}}
+  end
+
+  defp maybe_add_job_settings(list, []), do: list
+
+  defp maybe_add_job_settings(list, settings) do
+    list |> Keyword.put(:job_settings, settings)
   end
 
   defp validate_update_params(params) do
