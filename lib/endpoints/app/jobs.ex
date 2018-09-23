29226092a71636de4c8d3ae671076c766e15cea7
David Sulc <davidsulc@gmail.com>
2018-09-23 14:42:24 +0200

    added opts to request

--- lib/endpoints/app/jobs.ex
+++ lib/endpoints/app/jobs.ex
@@ -11,14 +11,13 @@ defmodule SHEx.Endpoints.App.Jobs do
       when is_project_id(project_id)
       when is_list(jobs)
       when is_list(opts) do
-    http_client = get_http_client(opts)
-
     RequestConfig.new()
     |> Map.put(:api_key, api_key)
     |> Map.put(:url, "#{@base_url}/jobs/delete.json?project=#{project_id}")
     |> Map.put(:method, :post)
     |> Map.put(:body, jobs |> Enum.map(&{:job, &1}))
-    |> http_client.request()
+    |> Map.put(:opts, opts)
+    |> make_request()
   end
 
   def stop(api_key, project_id, jobs, opts \\ [])
@@ -26,14 +25,19 @@ defmodule SHEx.Endpoints.App.Jobs do
       when is_project_id(project_id)
       when is_list(jobs)
       when is_list(opts) do
-    http_client = get_http_client(opts)
-
     RequestConfig.new()
     |> Map.put(:api_key, api_key)
     |> Map.put(:url, "#{@base_url}/jobs/stop.json?project=#{project_id}")
     |> Map.put(:method, :post)
     |> Map.put(:body, jobs |> Enum.map(&{:job, &1}))
-    |> http_client.request()
+    |> Map.put(:opts, opts)
+    |> make_request()
+  end
+
+  defp make_request(%RequestConfig{opts: opts} = config) do
+    http_client = get_http_client(opts)
+
+    config |> http_client.request()
   end
 
   defp get_http_client(opts) do
