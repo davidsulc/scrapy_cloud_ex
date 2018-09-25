ccec2d66a2db4a17e9e657b4c7f877392f6e39c7
David Sulc <davidsulc@gmail.com>
2018-09-25 08:06:07 +0200

    checkpoint: initial implementation of Items.get

--- lib/endpoints/storage.ex
+++ lib/endpoints/storage.ex
@@ -1,5 +1,16 @@
 defmodule SHEx.Endpoints.Storage do
   def pagination_params(), do: [:count, :index, :start, :startafter]
+
   def csv_params(), do: [:fields, :include_headers, :sep, :quote, :escape, :lineend]
+
   def meta_params(), do: [:_key, :_ts]
+
+  def all_params() do
+    pagination_params() ++ csv_params() ++ meta_params()
+  end
+
+  @valid_formats [:json, :jl, :xml, :csv, :text]
+  def validate_format(nil), do: :ok
+  def validate_format(format) when format in @valid_formats, do: :ok
+  def validate_format(format), do: {:invalid_format, {"expected format '#{format}' to be one of: #{@valid_formats}"}}
 end
--- lib/endpoints/storage/items.ex
+++ lib/endpoints/storage/items.ex
@@ -1,31 +1,75 @@
 defmodule SHEx.Endpoints.Storage.Items do
+  require Logger
   import SHEx.Endpoints.Guards
 
-  alias SHEx.Endpoints.Helpers
+  alias SHEx.Endpoints.{Helpers, Storage}
   alias SHEx.HttpAdapter.RequestConfig
 
   @base_url "https://storage.scrapinghub.com/items"
 
-  def stats(api_key, project_id, spider_id, job_id, opts \\ [])
+  def get(api_key, composite_id, params \\ [], opts \\ [])
       when is_api_key(api_key)
-      when is_id(project_id)
-      when is_id(spider_id)
-      when is_id(job_id)
+      when is_binary(composite_id)
+      when is_list(params)
       when is_list(opts) do
-    complete_job_id = [project_id, spider_id, job_id] |> to_complete_job_id()
-    stats(api_key, complete_job_id, opts)
+        with valid_params <- [:item_index, :field_name, :format, :meta, :nodata] ++ Storage.all_params(),
+         :ok <- Helpers.validate_params(params, valid_params),
+         :ok <- Storage.validate_format(params |> Keyword.get(:format)),
+         item_index <- Keyword.get(params, :item_index, ""),
+         {:item_index, :ok} <- {:item_index, validate_optional_param_type(item_index, &(is_integer(&1) || is_binary(&1)))},
+         field_name <- Keyword.get(params, :field_name, ""),
+         {:field_name, :ok} <- {:field_name, validate_optional_param_type(item_index, &is_binary/1)} do
+      base_url =
+        [@base_url, composite_id, item_index, field_name]
+        |> Enum.reject(& &1 == "")
+        |> merge_sections()
+
+      pagination_params =
+        params
+        |> Enum.filter(fn {k, _} -> Enum.member?(Storage.pagination_params(), k) end)
+        |> warn_on_empty_pagination_params()
+
+      query = URI.encode_query(pagination_params)
+
+      url = "#{base_url}?#{query}"
+
+      RequestConfig.new()
+      |> Map.put(:api_key, api_key)
+      |> Map.put(:opts, opts)
+      |> Map.put(:url, url)
+      # |> Map.put(:headers, [{:"Accept", "application/json"}])
+      |> Helpers.make_request()
+    else
+      {:item_index, :error} -> {:error, {:invalid_param, {:item_index, "item index must be an integer or a string"}}}
+      {:field_name, :error} -> {:error, {:invalid_param, {:field_name, "field name must be a string"}}}
+    end
   end
 
-  def stats(api_key, complete_job_id, opts \\ [])
+  def stats(api_key, composite_id, opts \\ [])
       when is_api_key(api_key)
-      when is_binary(complete_job_id)
+      when is_binary(composite_id)
       when is_list(opts) do
     RequestConfig.new()
     |> Map.put(:api_key, api_key)
     |> Map.put(:opts, opts)
-    |> Map.put(:url, "#{@base_url}/#{complete_job_id}/stats")
+    |> Map.put(:url, [@base_url, composite_id, "stats"] |> merge_sections())
     |> Helpers.make_request()
   end
 
-  def to_complete_job_id([project_id, spider_id, job_id]), do: "#{project_id}/#{spider_id}/#{job_id}"
+  defp merge_sections(sections), do: sections |> Enum.join("/")
+
+  defp validate_optional_param_type("", _), do: :ok
+  defp validate_optional_param_type(param, validator) do
+    if validator.(param) do
+      :ok
+    else
+      :error
+    end
+  end
+
+  defp warn_on_empty_pagination_params([_ | _] = params), do: params
+  defp warn_on_empty_pagination_params([]) do
+    Logger.warn("#{__MODULE__}.get/4 called without pagination params")
+    []
+  end
 end
