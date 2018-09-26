3c564ac8fd1ec9cb066ed8e6b75792bc46cf740f
David Sulc <davidsulc@gmail.com>
2018-09-26 19:28:47 +0200

    fixed error message

--- lib/endpoints/storage.ex
+++ lib/endpoints/storage.ex
@@ -12,5 +12,5 @@ defmodule SHEx.Endpoints.Storage do
   @valid_formats [:json, :jl, :xml, :csv, :text]
   def validate_format(nil), do: :ok
   def validate_format(format) when format in @valid_formats, do: :ok
-  def validate_format(format), do: {:invalid_param, {:format, "expected format '#{format}' to be one of: #{@valid_formats}"}}
+  def validate_format(format), do: {:invalid_param, {:format, "expected format '#{inspect(format)}' to be one of: #{inspect(@valid_formats)}"}}
 end
