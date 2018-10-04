f7fcf5486af65bcd00ff59206a6f040306c14d6b
David Sulc <davidsulc@gmail.com>
2018-10-04 08:12:53 +0200

    implemented Activity.projects

--- lib/endpoints/storage/activity.ex
+++ lib/endpoints/storage/activity.ex
@@ -1,7 +1,7 @@
 defmodule ScrapingHubEx.Endpoints.Storage.Activity do
   import ScrapingHubEx.Endpoints.Guards
 
-  alias ScrapingHubEx.Endpoints.Helpers
+  alias ScrapingHubEx.Endpoints.{Helpers, Storage}
   alias ScrapingHubEx.HttpAdapter.RequestConfig
 
   @base_url "https://storage.scrapinghub.com/activity"
@@ -34,10 +34,46 @@ defmodule ScrapingHubEx.Endpoints.Storage.Activity do
     end
   end
 
+  def projects(api_key, params \\ [], opts \\ [])
+      when is_api_key(api_key)
+      when is_list(params)
+      when is_list(opts) do
+    params =
+      params
+      |> set_default_format()
+      |> Helpers.canonicalize_params(@param_synonyms)
+
+    with :ok <- Helpers.validate_params(params, [:count, :p, :pcount, :meta, :format]),
+         :ok <- Helpers.validate_params(Keyword.get(params, :meta, []), [:_project | Storage.meta_params()]) do
+      base_url = [@base_url, "projects"] |> Enum.join("/")
+
+      query_string =
+        params
+        |> process_meta_params()
+        |> URI.encode_query()
+
+      RequestConfig.new()
+      |> Map.put(:api_key, api_key)
+      |> Map.put(:url, "#{base_url}?#{query_string}")
+      |> Map.put(:headers, Keyword.get(opts, :headers, []))
+      |> Map.put(:opts, opts |> Keyword.put(:decoder_format, Keyword.get(params, :format, :json)))
+      |> Helpers.make_request()
+    else
+      error -> {:error, error}
+    end
+  end
+
   defp set_default_format(params) do
     case Keyword.get(params, :format) do
       nil -> Keyword.put(params, :format, @default_format)
       _ -> params
+    end
+  end
+
+  defp process_meta_params(params) do
+    case Keyword.get(params, :meta) do
+      nil -> params
+      values -> Enum.map(values, &{:meta, &1}) ++ Keyword.delete(params, :meta)
     end
   end
 end
