ab9efce70c565533c3febb7b5bb943af0f2214ec
David Sulc <davidsulc@gmail.com>
2018-10-04 22:05:35 +0200

    finished implementing App.Comments API

--- lib/endpoints/app/comments.ex
+++ lib/endpoints/app/comments.ex
@@ -13,6 +13,14 @@ defmodule ScrapyCloudEx.Endpoints.App.Comments do
     basic_comment_request(api_key, composite_id, [], opts, :get)
   end
 
+  def put(api_key, composite_id, params \\ [], opts \\ [])
+      when is_api_key(api_key)
+      when is_binary(composite_id) and composite_id != ""
+      when is_list(params)
+      when is_list(opts) do
+    basic_comment_request(api_key, composite_id, params, opts, :put)
+  end
+
   def post(api_key, composite_id, params \\ [], opts \\ [])
       when is_api_key(api_key)
       when is_binary(composite_id) and composite_id != ""
@@ -31,7 +39,7 @@ defmodule ScrapyCloudEx.Endpoints.App.Comments do
 
   defp basic_comment_request(api_key, composite_id, params, opts, method) do
     with :ok <- Helpers.validate_params(params, [:text]),
-         true <- method == :get || split_sections(composite_id) > 3 do
+         :ok <- check_constraints(method, composite_id, params) do
       RequestConfig.new()
       |> Map.put(:api_key, api_key)
       |> Map.put(:method, method)
@@ -41,7 +49,6 @@ defmodule ScrapyCloudEx.Endpoints.App.Comments do
       |> Helpers.make_request()
     else
       {:invalid_param, _} = error -> {:error, error}
-      false -> {:error, {:invalid_param, {:composite_id, "expected id to contain at least 4 sections"}}}
     end
   end
 
@@ -56,7 +63,54 @@ defmodule ScrapyCloudEx.Endpoints.App.Comments do
     |> Helpers.make_request()
   end
 
+  defp check_constraints(method, composite_id, params)
+      when is_atom(method)
+      when is_binary(composite_id) or is_integer(composite_id)
+      when is_list(params) do
+    do_check_constraints(method, section_count(composite_id), Keyword.has_key?(params, :text))
+  end
+
+  # comments/:project_id/:spider_id/:job_id
+  defp do_check_constraints(:get, 3, _), do: :ok
+
+  # comments/:project_id/:spider_id/:job_id/:item_no[/:field]
+  defp do_check_constraints(:get, count, _) when count > 3, do: :ok
+
+  defp do_check_constraints(:get, _, _) do
+    "expected `id` param to have at least 3 sections"
+    |> Helpers.invalid_param_error(:id)
+  end
+
+  # comments/:comment_id
+  defp do_check_constraints(:put, _, true), do: :ok
+
+  defp do_check_constraints(:put, _, false), do: required_text_param_not_provided()
+
+  # comments/:project_id/:spider_id/:job_id/:item_no[/:field]
+  defp do_check_constraints(:post, count, _) when count > 3, do: :ok
+
+  defp do_check_constraints(:post, _, _) do
+    "expected `id` param to have at least 4 sections"
+    |> Helpers.invalid_param_error(:id)
+  end
+
+  # comments/:comment_id
+  defp do_check_constraints(:delete, 1, _), do: :ok
+
+  # comments/:project_id/:spider_id/:job_id/:item_no[/:field]
+  defp do_check_constraints(:delete, count, _) when count > 3, do: :ok
+
+  defp do_check_constraints(:delete, _, _) do
+    "expected `id` param to have only 1 section, or at least 4 sections"
+    |> Helpers.invalid_param_error(:id)
+  end
+
+  defp required_text_param_not_provided() do
+    "required `text` param not provided" |> Helpers.invalid_param_error(:text)
+  end
+
   defp merge_sections(sections), do: sections |> Enum.join("/")
 
-  defp split_sections(composite), do: composite |> String.split("/")
+  defp section_count(composite) when is_integer(composite), do: 1
+  defp section_count(composite) when is_binary(composite), do: composite |> String.split("/") |> length()
 end
