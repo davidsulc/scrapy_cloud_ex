84f1ac30979b87c9561ac08ac05a701cb7c8255a
David Sulc <davidsulc@gmail.com>
2018-10-04 07:31:00 +0200

    refactored

--- lib/endpoints/storage/job_q.ex
+++ lib/endpoints/storage/job_q.ex
@@ -4,6 +4,8 @@ defmodule ScrapingHubEx.Endpoints.Storage.JobQ do
   alias ScrapingHubEx.Endpoints.Helpers
   alias ScrapingHubEx.HttpAdapter.RequestConfig
 
+  @base_url "https://storage.scrapinghub.com/jobq"
+
   @default_format :json
 
   @param_synonyms [
@@ -13,29 +15,14 @@ defmodule ScrapingHubEx.Endpoints.Storage.JobQ do
     {:lackstag, :lacks_tag}
   ]
 
-  @base_url "https://storage.scrapinghub.com/jobq"
+  @valid_params [:spider, :state, :startts, :endts, :has_tag, :lacks_tag, :format]
 
   def count(api_key, project_id, params \\ [], opts \\ [])
       when is_api_key(api_key)
       when is_binary(project_id) and project_id != ""
       when is_list(params)
       when is_list(opts) do
-    valid_params = [:spider, :state, :startts, :endts, :has_tag, :lacks_tag]
-    params = params |> Helpers.canonicalize_params(@param_synonyms)
-
-    with :ok <- Helpers.validate_params(params, valid_params) do
-      base_url = [@base_url, project_id, "count"] |> Enum.join("/")
-      query_string = URI.encode_query(params)
-
-      RequestConfig.new()
-      |> Map.put(:api_key, api_key)
-      |> Map.put(:url, "#{base_url}?#{query_string}")
-      |> Map.put(:headers, Keyword.get(opts, :headers, []))
-      |> Map.put(:opts, opts |> Keyword.put(:decoder_format, :json))
-      |> Helpers.make_request()
-    else
-      error -> {:error, error}
-    end
+    make_request(api_key, project_id, params, opts, @valid_params, "count")
   end
 
   def list(api_key, project_id, params \\ [], opts \\ [])
@@ -43,15 +30,18 @@ defmodule ScrapingHubEx.Endpoints.Storage.JobQ do
       when is_binary(project_id) and project_id != ""
       when is_list(params)
       when is_list(opts) do
+    valid_params = @valid_params ++ [:start, :stop, :key]
+    make_request(api_key, project_id, params, opts, valid_params, "list")
+  end
+
+  defp make_request(api_key, project_id, params, opts, valid_params, endpoint) do
     params =
       params
       |> set_default_format()
       |> Helpers.canonicalize_params(@param_synonyms)
 
-    valid_params = [:spider, :state, :startts, :endts, :start, :stop, :key, :has_tag, :lacks_tag, :format]
-
     with :ok <- Helpers.validate_params(params, valid_params) do
-      base_url = [@base_url, project_id, "list"] |> Enum.join("/")
+      base_url = [@base_url, project_id, endpoint] |> Enum.join("/")
       query_string = URI.encode_query(params)
 
       RequestConfig.new()
