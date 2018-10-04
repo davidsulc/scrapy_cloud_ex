005d2819b7ea5801526ae41cd72dbb63f2bb0658
David Sulc <davidsulc@gmail.com>
2018-10-04 21:15:51 +0200

    implemented Comments.post for composite id w/ 4 sections or more

--- lib/endpoints/app/comments.ex
+++ lib/endpoints/app/comments.ex
@@ -6,6 +6,37 @@ defmodule ScrapyCloudEx.Endpoints.App.Comments do
 
   @base_url "https://app.scrapinghub.com/api/comments"
 
+  def get(api_key, composite_id, opts \\ [])
+      when is_api_key(api_key)
+      when is_binary(composite_id) and composite_id != ""
+      when is_list(opts) do
+    RequestConfig.new()
+    |> Map.put(:api_key, api_key)
+    |> Map.put(:opts, opts |> Keyword.put(:decoder_format, :json))
+    |> Map.put(:url, [@base_url, composite_id] |> Enum.join("/"))
+    |> Helpers.make_request()
+  end
+
+  def post(api_key, composite_id, params \\ [], opts \\ [])
+      when is_api_key(api_key)
+      when is_binary(composite_id) and composite_id != ""
+      when is_list(params)
+      when is_list(opts) do
+    with :ok <- Helpers.validate_params(params, [:text]),
+         true <- split_sections(composite_id) > 3 do
+      RequestConfig.new()
+      |> Map.put(:api_key, api_key)
+      |> Map.put(:method, :post)
+      |> Map.put(:body, params)
+      |> Map.put(:opts, opts |> Keyword.put(:decoder_format, :json))
+      |> Map.put(:url, [@base_url, composite_id] |> Enum.join("/"))
+      |> Helpers.make_request()
+    else
+      {:invalid_param, _} = error -> {:error, error}
+      false -> {:error, {:invalid_param, {:composite_id, "expected id to contain at least 4 sections"}}}
+    end
+  end
+
   def stats(api_key, project_id, opts \\ [])
       when is_api_key(api_key)
       when is_binary(project_id)
@@ -18,4 +49,6 @@ defmodule ScrapyCloudEx.Endpoints.App.Comments do
   end
 
   defp merge_sections(sections), do: sections |> Enum.join("/")
+
+  defp split_sections(composite), do: composite |> String.split("/")
 end
