8c7e99f7293a19f49fc2d7fc879ebe1ff91ee70a
David Sulc <davidsulc@gmail.com>
2018-10-07 18:05:01 +0200

    added Storage.JobQ tests

--- lib/endpoints/storage/job_q.ex
+++ lib/endpoints/storage/job_q.ex
@@ -13,7 +13,7 @@ defmodule ScrapyCloudEx.Endpoints.Storage.JobQ do
     {:end_ts, :endts}
   ]
 
-  @valid_params [:spider, :state, :startts, :endts, :has_tag, :lacks_tag, :format]
+  @valid_params [:spider, :state, :startts, :endts, :has_tag, :lacks_tag]
 
   def count(api_key, project_id, params \\ [], opts \\ [])
       when is_api_key(api_key)
@@ -28,15 +28,12 @@ defmodule ScrapyCloudEx.Endpoints.Storage.JobQ do
       when is_binary(project_id) and project_id != ""
       when is_list(params)
       when is_list(opts) do
-    valid_params = @valid_params ++ [:start, :stop, :key]
-    make_request(api_key, project_id, params, opts, valid_params, "list")
+    valid_params = @valid_params ++ [:format, :start, :stop, :key]
+    make_request(api_key, project_id, params |> set_default_format(), opts, valid_params, "list")
   end
 
   defp make_request(api_key, project_id, params, opts, valid_params, endpoint) do
-    params =
-      params
-      |> set_default_format()
-      |> Helpers.canonicalize_params(@param_synonyms)
+    params = params |> Helpers.canonicalize_params(@param_synonyms)
 
     with :ok <- Helpers.validate_params(params, valid_params) do
       base_url = [@base_url, project_id, endpoint] |> Enum.join("/")
