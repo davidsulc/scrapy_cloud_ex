1287bf626220a2a915f06c8a84b26eebf44305c7
David Sulc <davidsulc@gmail.com>
2018-10-07 20:19:59 +0200

    added tests for Storage.Requests

--- lib/endpoints/storage/requests.ex
+++ lib/endpoints/storage/requests.ex
@@ -5,7 +5,7 @@ defmodule ScrapyCloudEx.Endpoints.Storage.Requests do
   alias ScrapyCloudEx.Endpoints.Storage.QueryParams
   alias ScrapyCloudEx.HttpAdapter.RequestConfig
 
-  @base_url "https://storage.scrapinghub.com/logs"
+  @base_url "https://storage.scrapinghub.com/requests"
 
   def get(api_key, composite_id, params \\ [], opts \\ [])
       when is_api_key(api_key)
@@ -15,6 +15,7 @@ defmodule ScrapyCloudEx.Endpoints.Storage.Requests do
     with %QueryParams{error: nil} = query_params <- params |> QueryParams.from_keywords() do
       query_string = query_params |> QueryParams.to_query()
       base_url = [@base_url, composite_id] |> Enum.join("/")
+      opts = opts |> Helpers.set_default_decoder_format(Keyword.get(params, :format))
 
       RequestConfig.new()
       |> RequestConfig.put(:api_key, api_key)
@@ -32,10 +33,16 @@ defmodule ScrapyCloudEx.Endpoints.Storage.Requests do
       when is_api_key(api_key)
       when is_binary(composite_id)
       when is_list(opts) do
-    RequestConfig.new()
-    |> RequestConfig.put(:api_key, api_key)
-    |> RequestConfig.merge_opts(opts)
-    |> RequestConfig.put(:url, [@base_url, composite_id, "stats"] |> Enum.join("/"))
-    |> Helpers.make_request()
+    with 3 <- composite_id |> String.split("/") |> length() do
+      RequestConfig.new()
+      |> RequestConfig.put(:api_key, api_key)
+      |> RequestConfig.merge_opts(opts)
+      |> RequestConfig.put(:url, [@base_url, composite_id, "stats"] |> Enum.join("/"))
+      |> Helpers.make_request()
+    else
+      _ ->
+        error = "expected `id` param to have exactly 3 sections" |> Helpers.invalid_param_error(:id)
+        {:error, error}
+    end
   end
 end
