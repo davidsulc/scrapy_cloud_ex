15e1099df043c164370f75e63d2305accb628250
David Sulc <davidsulc@gmail.com>
2018-10-10 20:24:05 +0200

    added test for RequestConfig attribute types

--- lib/http_adapter/request_config.ex
+++ lib/http_adapter/request_config.ex
@@ -1,4 +1,6 @@
 defmodule ScrapyCloudEx.HttpAdapter.RequestConfig do
+  @http_methods [:get, :post, :put, :delete]
+
   defstruct [
     :api_key,
     :url,
@@ -13,8 +15,32 @@ defmodule ScrapyCloudEx.HttpAdapter.RequestConfig do
 
   def new(), do: %__MODULE__{}
 
-  def merge_opts(%__MODULE__{opts: opts} = struct, new_opts) do
+  def merge_opts(%__MODULE__{opts: opts} = struct, new_opts) when is_list(new_opts) do
     %{struct | opts: Keyword.merge(opts, new_opts)}
+  end
+
+  def put(%__MODULE__{} = config, key, value) when key in [:api_key, :url] and is_binary(value) do
+    config |> Map.put(key, value)
+  end
+
+  def put(%__MODULE__{}, key, _) when key in [:api_key, :url] do
+    raise ArgumentError, message: "value for key '#{key}' must be a string"
+  end
+
+  def put(%__MODULE__{} = config, :method, method) when method in @http_methods do
+    config |> Map.put(:method, method)
+  end
+
+  def put(%__MODULE__{}, :method, _) do
+    raise ArgumentError, message: "method must be one of #{inspect(@http_methods)}"
+  end
+
+  def put(%__MODULE__{} = config, key, value) when key in [:headers, :body] do
+    if is_tuple_list?(value) do
+      config |> Map.put(key, value)
+    else
+      raise ArgumentError, message: "value for key '#{key}' must be a list of tuples (such as a keyword list)"
+    end
   end
 
   # so that default opts don't inadvertently get removed without a replacement value
@@ -22,5 +48,14 @@ defmodule ScrapyCloudEx.HttpAdapter.RequestConfig do
     raise ArgumentError, message: "use #{__MODULE__}.merge_opts/2 to add options"
   end
 
-  def put(%__MODULE__{} = struct, key, value), do: Map.put(struct, key, value)
+  def put(%__MODULE__{}, _, _) do
+    valid_keys = new() |> Map.keys()
+    raise ArgumentError, message: "key must be one of #{inspect(valid_keys)}"
+  end
+
+  defp is_tuple_list?([]), do: true
+
+  defp is_tuple_list?([{_, _} | t]), do: is_tuple_list?(t)
+
+  defp is_tuple_list?(_), do: false
 end
--- test/http_adapter/request_config_test.exs
+++ test/http_adapter/request_config_test.exs
@@ -35,15 +35,60 @@ defmodule ScrapyCloudEx.HttpAdapter.RequestConfigTest do
   end
 
   describe "put/3" do
+    test ":api_key only accepts a string", %{config: config} do
+      assert_raise ArgumentError, ~r/value for key 'api_key' must be a string/, fn ->
+        config |> RC.put(:api_key, :foo)
+      end
+      assert %RC{} = RC.put(config, :api_key, "123asd")
+    end
+
+    test ":url only accepts a string", %{config: config} do
+      assert_raise ArgumentError, ~r/value for key 'url' must be a string/, fn ->
+        config |> RC.put(:url, :foo)
+      end
+      assert %RC{} = RC.put(config, :url, "www.example.com")
+    end
+
+    test ":method only accepts an atom corresponding to http verbs", %{config: config} do
+      for method <- [:foo, "foo"] do
+        assert_raise ArgumentError, ~r/method must be one of/, fn ->
+          config |> RC.put(:method, method)
+        end
+      end
+
+      for method <- [:get, :post, :put, :delete] do
+        assert %RC{} = RC.put(config, :method, method)
+      end
+    end
+
+    test ":headers only accepts a list of tuples", %{config: config} do
+      assert_raise ArgumentError, ~r/must be a list of tuples/, fn ->
+        config |> RC.put(:headers, %{a: :b, foo: :bar})
+      end
+
+      assert %RC{} = RC.put(config, :headers, [a: :b, foo: :bar])
+      assert %RC{} = RC.put(config, :headers, [{"a", "b"}, {"foo", "bar"}])
+    end
+
+    test ":body only accepts a list of tuples", %{config: config} do
+      assert_raise ArgumentError, ~r/must be a list of tuples/, fn ->
+        config |> RC.put(:body, %{a: :b, foo: :bar})
+      end
+
+      assert %RC{} = RC.put(config, :body, [a: :b, foo: :bar])
+      assert %RC{} = RC.put(config, :body, [{"a", "b"}, {"foo", "bar"}])
+    end
+
     test "raises if trying to overwrite :opts", %{config: config} do
       assert_raise ArgumentError, ~r/merge_opts\/2 to add options$/, fn ->
         config |> RC.put(:opts, foo: :bar)
       end
     end
 
-    test "behaves like Map.put/3 for keys that aren't :opts", %{config: config} do
-      config = config |> RC.put(:url, "www.example.com")
-      assert Map.get(config, :url) == "www.example.com"
+    test "raises for keys that aren't in the RequestConfig struct", %{config: config} do
+      assert_raise ArgumentError, ~r/key must be one of/, fn ->
+        config |> RC.put(:foo, :bar)
+      end
     end
   end
 end
