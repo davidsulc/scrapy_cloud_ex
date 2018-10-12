169f578d34c7e69e7c9f129995ad64fca29e0202
David Sulc <davidsulc@gmail.com>
2018-10-12 20:20:46 +0200

    refactor

--- test/http_adapters/default_test.exs
+++ test/http_adapters/default_test.exs
@@ -22,34 +22,45 @@ defmodule ScrapyCloudEx.HttpAdapters.DefaultTest do
 
   defp decode_response(%Response{body: body}), do: Jason.decode(body)
 
-  describe "Http@adapter.request/1: GET requests" do
+  defp test_success(request) do
+    assert {:ok, _} = @adapter.request(request)
+  end
+
+  defp test_http_auth(request) do
+    key = "API_KEY"
+    base_64_auth = "#{key}:" |> Base.encode64()
+
+    response =
+      request
+      |> RequestConfig.put(:api_key, key)
+      |> @adapter.request()
+      |> extract_and_decode()
+
+    assert response["headers"]["authorization"] == "Basic #{base_64_auth}"
+  end
+
+  defp test_included_headers(request) do
+    response =
+      request
+      |> RequestConfig.put(:headers, [{"x-foo", "bar"}, {:another, :header}])
+      |> @adapter.request()
+      |> extract_and_decode()
+
+    assert response["headers"]["x-foo"] == "bar"
+    assert response["headers"]["another"] == "header"
+  end
+
+  describe "request/1: GET requests" do
     test "are successful" do
-      request = get_request()
-      assert {:ok, _} = @adapter.request(request)
+      get_request() |> test_success()
     end
 
     test "use HTTP Basic authentication" do
-      key = "API_KEY"
-      base_64_auth = "#{key}:" |> Base.encode64()
-
-      response =
-        get_request()
-        |> RequestConfig.put(:api_key, key)
-        |> @adapter.request()
-        |> extract_and_decode()
-
-      assert response["headers"]["authorization"] == "Basic #{base_64_auth}"
+      get_request() |> test_http_auth()
     end
 
     test "add included headers" do
-      response =
-        get_request()
-        |> RequestConfig.put(:headers, [{"x-foo", "bar"}, {:another, :header}])
-        |> @adapter.request()
-        |> extract_and_decode()
-
-      assert(response["headers"]["x-foo"] == "bar")
-      assert response["headers"]["another"] == "header"
+      get_request() |> test_included_headers()
     end
   end
 end
