c385fe9ae01ba8582fd78cd5fb5fd3b76ed68afa
David Sulc <davidsulc@gmail.com>
2018-10-13 13:34:18 +0200

    correct formats
    html should be accepted to allow (e.g.) getting/decoding specific item fields that contain HTML
    
    Don't consider text/plain as jl: certain API endpoints correctly set the content type to text/plain to return text (e.g. fetching an item field containing text). A workaround for the incorrect content type given by
    
    `"API_KEY" |> ScrapyCloudEx.Endpoints.App.Jobs.list(123, format: :jl)`
    
    will be implemented later. This example endpoint has "text/plain" in the
    content type header it returns, although the content is in fact json
    lines.

--- lib/http_adapter/helpers.ex
+++ lib/http_adapter/helpers.ex
@@ -1,11 +1,11 @@
 defmodule ScrapyCloudEx.HttpAdapter.Helpers do
   def get_format([]), do: :text
-  def get_format([{"Content-Type", "application/x-jsonlines"<>_} | _]), do: :jl
-  # e.g. "API_KEY" |> ScrapyCloudEx.Endpoints.App.Jobs.list("123", format: :jl)
-  def get_format([{"Content-Type", "text/plain"<>_} | _]), do: :jl
   def get_format([{"Content-Type", "application/json"<>_} | _]), do: :json
+  def get_format([{"Content-Type", "application/x-jsonlines"<>_} | _]), do: :jl
   def get_format([{"Content-Type", "application/xml"<>_} | _]), do: :xml
   def get_format([{"Content-Type", "text/csv"<>_} | _]), do: :csv
+  def get_format([{"Content-Type", "text/html"<>_} | _]), do: :html
+  def get_format([{"Content-Type", "text/plain"<>_} | _]), do: :text
   def get_format([_ | t]), do: get_format(t)
 
   def get_decoder_fun(decoder_fun) when is_function(decoder_fun), do: decoder_fun
