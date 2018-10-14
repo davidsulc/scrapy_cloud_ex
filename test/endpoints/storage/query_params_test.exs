cc220c7f85b7f2bc51a4ac37166efd3dfe36d5a9
David Sulc <davidsulc@gmail.com>
2018-10-14 10:21:27 +0200

    added tests for QueryParams.to_query/1

--- test/endpoints/storage/query_params_test.exs
+++ test/endpoints/storage/query_params_test.exs
@@ -5,6 +5,24 @@ defmodule ScrapyCloudEx.Endpoints.Storage.QueryParamsTest do
 
   alias ScrapyCloudEx.Endpoints.Storage.QueryParams
 
+  @full_params [
+    format: :csv,
+    csv: [
+      fields: ["field_1", "field_2"],
+      include_headers: 1,
+      sep: ", ",
+      quote: "\"",
+      escape: "@",
+      lineend: "\n"
+    ],
+    pagination: [
+      count: 3,
+      start: "1/2/3/4"
+    ],
+    meta: [:_ts, :_key],
+    nodata: 1
+  ]
+
   describe "warn_if_no_pagination/2" do
     test "returns the original QueryParams struct when pagination params present" do
       params = QueryParams.from_keywords([format: :xml, pagination: [count: 3]])
@@ -18,6 +36,43 @@ defmodule ScrapyCloudEx.Endpoints.Storage.QueryParamsTest do
       assert capture_log(fn ->
         assert QueryParams.warn_if_no_pagination(params, "foo/1") == params
       end) =~ "foo/1 called without pagination params or index"
+    end
+  end
+
+  describe "to_query/1" do
+    test "encodes the QueryParams object as a query string" do
+      params =
+        @full_params
+        |> QueryParams.from_keywords()
+        |> QueryParams.to_query()
+        |> URI.query_decoder()
+        |> Enum.map(fn {k, v} -> {:"#{k}", v} end)
+
+      expected_results =
+        [
+          {:fields, "field_1,field_2"},
+          {:include_headers, "1"},
+          {:sep, ", "},
+          {:quote, "\""},
+          {:escape, "@"},
+          {:lineend, "\n"},
+          {:format, "csv"},
+          {:meta, "_ts"},
+          {:meta, "_key"},
+          {:nodata, "1"},
+          {:count, "3"},
+          {:start, "1/2/3/4"}
+        ]
+
+      expected_results
+      |> Keyword.keys()
+      |> Enum.each(fn key ->
+        assert Keyword.get_values(params, key) == Keyword.get_values(expected_results, key)
+      end)
+    end
+
+    test "returns an error if the QueryParams struct contains one" do
+      assert {:error, :some_error} = %QueryParams{error: :some_error} |> QueryParams.to_query()
     end
   end
 end
