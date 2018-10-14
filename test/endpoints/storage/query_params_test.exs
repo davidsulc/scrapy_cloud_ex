02e820f3daaef1f94a15982561ccc994da228317
David Sulc <davidsulc@gmail.com>
2018-10-14 13:26:39 +0200

    refactor for better message on assert failure

--- test/endpoints/storage/query_params_test.exs
+++ test/endpoints/storage/query_params_test.exs
@@ -23,6 +23,17 @@ defmodule ScrapyCloudEx.Endpoints.Storage.QueryParamsTest do
     nodata: 1
   ]
 
+  defp assert_no_error(%QueryParams{} = params), do: assert %QueryParams{error: nil} = params
+
+  defp assert_invalid_param(%QueryParams{} = params, [{k, v} | []]) do
+    assert %QueryParams{error: {:invalid_param, {^k, {^v, _}}}} = params
+  end
+
+  defp assert_invalid_param(%QueryParams{} = params, param) do
+    assert %QueryParams{error: {:invalid_param, {^param, _}}} = params
+  end
+
+
   describe "from_keywords/1" do
     test "creates a QueryParams struct from a keyword list" do
       assert %QueryParams{} = QueryParams.from_keywords(@full_params)
@@ -94,19 +105,13 @@ defmodule ScrapyCloudEx.Endpoints.Storage.QueryParamsTest do
   end
 
   describe "from_keywords/1 validations:" do
-    defp has_error?(%QueryParams{error: nil}), do: false
-    defp has_error?(%QueryParams{error: _}), do: true
-
-    defp invalid_param?(%QueryParams{error: {:invalid_param, {param, _}}}, param), do: true
-    defp invalid_param?(%QueryParams{}, _param), do: false
-
     test "format" do
       [:json, :jl, :xml, :text, :html]
       |> Enum.each(fn format ->
-        refute QueryParams.from_keywords(format: format) |> has_error?()
+        QueryParams.from_keywords(format: format) |> assert_no_error()
       end)
 
-      refute QueryParams.from_keywords(format: :csv, csv: [fields: ["field"]]) |> has_error?()
+      QueryParams.from_keywords(format: :csv, csv: [fields: ["field"]]) |> assert_no_error()
 
       %{error: error} = QueryParams.from_keywords(format: :foo)
       assert {:invalid_param, {:format, _}} = error
@@ -122,7 +127,7 @@ defmodule ScrapyCloudEx.Endpoints.Storage.QueryParamsTest do
         lineend: "\n"
       ]
 
-      refute QueryParams.from_keywords(format: :csv, csv: csv_params) |> has_error?()
+      QueryParams.from_keywords(format: :csv, csv: csv_params) |> assert_no_error()
 
       %{error: error} = QueryParams.from_keywords(format: :csv, csv: [{:foo, :bar} | csv_params])
       assert {:invalid_param, {:csv_param, {:foo, _}}} = error
@@ -134,15 +139,21 @@ defmodule ScrapyCloudEx.Endpoints.Storage.QueryParamsTest do
 
     test "meta" do
       meta = [:_ts, :_key]
-      refute QueryParams.from_keywords(meta: meta) |> has_error?()
+      QueryParams.from_keywords(meta: meta) |> assert_no_error()
 
-      assert QueryParams.from_keywords(meta: [{:foo, :bar} | meta]) |> invalid_param?(:meta)
-      assert QueryParams.from_keywords(meta: :_key) |> invalid_param?(:meta)
+      QueryParams.from_keywords(meta: [{:foo, :bar} | meta]) |> assert_invalid_param(:meta)
+      QueryParams.from_keywords(meta: :_key) |> assert_invalid_param(:meta)
     end
 
     test "nodata" do
-      refute QueryParams.from_keywords(nodata: 0) |> has_error?()
-      refute QueryParams.from_keywords(nodata: 1) |> has_error?()
+      QueryParams.from_keywords(nodata: 0) |> assert_no_error()
+      QueryParams.from_keywords(nodata: 1) |> assert_no_error()
+
+      QueryParams.from_keywords(nodata: 2) |> assert_invalid_param(:nodata)
+      QueryParams.from_keywords(nodata: :foo) |> assert_invalid_param(:nodata)
+    end
+  end
+
 
       assert QueryParams.from_keywords(nodata: 2) |> invalid_param?(:nodata)
       assert QueryParams.from_keywords(nodata: :foo) |> invalid_param?(:nodata)
