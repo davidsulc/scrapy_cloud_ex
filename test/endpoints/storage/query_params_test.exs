321ea29fd0560487551652f9d83e6a377ee8ac08
David Sulc <davidsulc@gmail.com>
2018-10-14 13:00:47 +0200

    added QueryParams validation tests

--- test/endpoints/storage/query_params_test.exs
+++ test/endpoints/storage/query_params_test.exs
@@ -93,6 +93,62 @@ defmodule ScrapyCloudEx.Endpoints.Storage.QueryParamsTest do
     end
   end
 
+  describe "from_keywords/1 validations:" do
+    defp has_error?(%QueryParams{error: nil}), do: false
+    defp has_error?(%QueryParams{error: _}), do: true
+
+    defp invalid_param?(%QueryParams{error: {:invalid_param, {param, _}}}, param), do: true
+    defp invalid_param?(%QueryParams{}, _param), do: false
+
+    test "format" do
+      [:json, :jl, :xml, :text, :html]
+      |> Enum.each(fn format ->
+        refute QueryParams.from_keywords(format: format) |> has_error?()
+      end)
+
+      refute QueryParams.from_keywords(format: :csv, csv: [fields: ["field"]]) |> has_error?()
+
+      %{error: error} = QueryParams.from_keywords(format: :foo)
+      assert {:invalid_param, {:format, _}} = error
+    end
+
+    test "CSV params" do
+      csv_params = [
+        fields: ~w(field_a field_b),
+        include_headers: 1,
+        sep: ", ",
+        quote: "\"",
+        escape: "@",
+        lineend: "\n"
+      ]
+
+      refute QueryParams.from_keywords(format: :csv, csv: csv_params) |> has_error?()
+
+      %{error: error} = QueryParams.from_keywords(format: :csv, csv: [{:foo, :bar} | csv_params])
+      assert {:invalid_param, {:csv_param, {:foo, _}}} = error
+
+      %{error: error} = QueryParams.from_keywords(format: :csv, csv: [])
+      assert {:invalid_param, {:csv_param, message}} = error
+      assert message =~ "required attribute 'fields' not provided"
+    end
+
+    test "meta" do
+      meta = [:_ts, :_key]
+      refute QueryParams.from_keywords(meta: meta) |> has_error?()
+
+      assert QueryParams.from_keywords(meta: [{:foo, :bar} | meta]) |> invalid_param?(:meta)
+      assert QueryParams.from_keywords(meta: :_key) |> invalid_param?(:meta)
+    end
+
+    test "nodata" do
+      refute QueryParams.from_keywords(nodata: 0) |> has_error?()
+      refute QueryParams.from_keywords(nodata: 1) |> has_error?()
+
+      assert QueryParams.from_keywords(nodata: 2) |> invalid_param?(:nodata)
+      assert QueryParams.from_keywords(nodata: :foo) |> invalid_param?(:nodata)
+    end
+  end
+
   describe "warn_if_no_pagination/2" do
     test "returns the original QueryParams struct when pagination params present" do
       params = QueryParams.from_keywords([format: :xml, pagination: [count: 3]])
