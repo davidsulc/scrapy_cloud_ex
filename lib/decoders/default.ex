29d7d32b49e9c9ebd66361eccdf93ecd2bdf07d4
David Sulc <davidsulc@gmail.com>
2018-11-02 18:26:26 +0100

    edited docs

--- lib/decoders/default.ex
+++ lib/decoders/default.ex
@@ -2,8 +2,10 @@ defmodule ScrapyCloudEx.Decoders.Default do
   @moduledoc """
   A default implementation of the `ScrapyCloudEx.Decoder` behaviour.
 
-  This decoder uses `Jason` to decode JSON values data, all other return types
+  Only decodes JSON values data, all other return types
   are simply forwarded without transformation.
+
+  Depends on `Jason`.
   """
 
   alias ScrapyCloudEx.Decoder
