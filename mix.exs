69c48afb71d307eb9d06bd75537f7989c4ff6b2c
David Sulc <davidsulc@gmail.com>
2018-11-03 15:26:05 +0100

    added package publication info

--- mix.exs
+++ mix.exs
@@ -1,18 +1,19 @@
 defmodule ScrapyCloudEx.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :scrapy_cloud_ex,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env()),
-
-      # Docs
-      name: "ScrapyCloudEx",
-      source_url: "https://github.com/davidsulc/scrapy_cloud_ex",
+      preferred_cli_env: [docs: :docs],
+      description: description(),
+      package: package(),
       docs: docs()
     ]
   end
@@ -30,13 +31,13 @@ defmodule ScrapyCloudEx.MixProject do
       {:hackney, "~> 1.14", only: [:dev, :test]},
       {:jason, "~> 1.1", only: [:dev, :test]},
       {:httparrot, "~> 1.0", only: :test},
-      {:dialyxir, "~> 1.0.0-rc.3", only: [:dev], runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.3", only: :dev, runtime: false},
       {:credo, "~> 0.10.0", only: [:dev, :test], runtime: false},
       # TODO update once v. > "0.19.1" is available
       {:ex_doc,
        git: "https://github.com/elixir-lang/ex_doc.git",
        ref: "f006883de3400e5e4fdfbe421b63ef919d3cf7ef",
-       only: :dev,
+       only: :docs,
        runtime: false}
     ]
   end
@@ -44,9 +45,25 @@ defmodule ScrapyCloudEx.MixProject do
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
+  defp description() do
+    """
+    An API wrapper for Scrapy Cloud API provided by ScrapingHub.com.
+    """
+  end
+
+  defp package() do
+    [
+      maintainers: ["David Sulc"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/davidsulc/scrapy_cloud_ex"}
+    ]
+  end
+
   defp docs() do
     [
-      main: "ScrapyCloudEx",
+      name: "ScrapyCloudEx",
+      source_url: "https://github.com/davidsulc/scrapy_cloud_ex",
+      main: "readme",
       extras: ["README.md"],
       groups_for_modules: [
         Endpoints: [
