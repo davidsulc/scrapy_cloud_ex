949fee86b0bd2c619fa36f09d3ab77461a368fb9
David Sulc <davidsulc@gmail.com>
2018-11-04 10:19:38 +0100

    ran formatter

--- config/config.exs
+++ config/config.exs
@@ -21,7 +21,7 @@ use Mix.Config
 #     config :logger, level: :info
 #
 
-if Mix.env() == :test, do: config :logger, level: :info
+if Mix.env() == :test, do: config(:logger, level: :info)
 
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
