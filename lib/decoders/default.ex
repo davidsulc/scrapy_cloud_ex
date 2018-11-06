e4a85376def2ca2bcb1671ea812714f83b02e132
David Sulc <davidsulc@gmail.com>
2018-11-06 06:59:04 +0100

    compile conditionally to handle optional deps

--- lib/decoders/default.ex
+++ lib/decoders/default.ex
@@ -1,22 +1,38 @@
-defmodule ScrapyCloudEx.Decoders.Default do
-  @moduledoc """
-  A default implementation of the `ScrapyCloudEx.Decoder` behaviour.
+if Code.ensure_loaded?(Jason) do
+  defmodule ScrapyCloudEx.Decoders.Default do
+    @moduledoc """
+    A default implementation of the `ScrapyCloudEx.Decoder` behaviour.
 
-  Only decodes JSON values data, all other return types
-  are simply forwarded without transformation.
+    Only decodes JSON values data, all other return types
+    are simply forwarded without transformation.
 
-  Depends on `Jason`.
-  """
+    Depends on `Jason`.
+    """
 
-  alias ScrapyCloudEx.Decoder
+    alias ScrapyCloudEx.Decoder
 
-  @behaviour Decoder
+    @behaviour Decoder
 
-  @impl Decoder
+    @impl Decoder
 
-  @spec decode(String.t(), atom) :: {:ok, any} | ScrapyCloudEx.tagged_error()
+    @spec decode(String.t(), atom) :: {:ok, any} | ScrapyCloudEx.tagged_error()
 
-  def decode(body, :json), do: Jason.decode(body)
+    def decode(body, :json) do
+      Jason.decode(body)
+    end
 
-  def decode(body, _), do: {:ok, body}
+    def decode(body, _), do: {:ok, body}
+  end
+else
+  defmodule ScrapyCloudEx.Decoders.Default do
+    @moduledoc false
+
+    @behaviour ScrapyCloudEx.Decoder
+
+    def decode(_, _) do
+      raise RuntimeError,
+        message:
+          "Jason is required to use #{__MODULE__}, refer to installation instructions in README"
+    end
+  end
 end
